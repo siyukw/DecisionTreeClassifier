e0afbd55df30ae85ca185e88e8590a9ddb7f93e9
Siyu Kat Wang <siyukw@gmail.com>
2017-06-14 13:12:22 -0700

    added helper, changed file type

---
+++ CalculateHelper.py
@@ -0,0 +1,7 @@
+import math
+from math import log
+### entropy = sum(i)[-Pi(log(Pi,2))]
+### information gain = entropy of parent - weighted entropy of children
+answer = 1 - 3 * 0.9184 / 4
+answer2 = - 1 * log(1, 2)
+print answer2
